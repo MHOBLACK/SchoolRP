978570e3a54aab9fdeddb86640c75e0f226fcd1d
Chatrtchai Chotsawat <chatrtchai.c@ku.th>
2024-08-19 01:17:37 +0700

    add 'sandbox' DeriveGamemode

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -3,6 +3,8 @@ GM.Author = "MHOBLACK"
 GM.Email = "modavidblack@gmail.com"
 GM.Website = "N/A"
 
+DeriveGamemode("sandbox")
+
 function GM:Initialize()
     
     self.BaseClass.Initialize(self)    
